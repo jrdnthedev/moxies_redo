f708f731ba33f2bb9a0e8a657982b3bf88d15440
smooooky <75350537+smooooky@users.noreply.github.com>
2023-06-06 14:04:48 -0400

    added transitions and styles to unique section image gallery

--- ts/main.ts
+++ ts/main.ts
@@ -2,6 +2,7 @@ let isToggle = false;
 
 const element = document.getElementById("hamburger_link");
 element?.addEventListener("click", toggle);
+const stacked = document.getElementById('stacked_image_wrapper');
 
 function toggle() {
     isToggle = !isToggle;
@@ -20,3 +21,27 @@ function toggle() {
     }
 
 }
+
+stacked?.addEventListener('mouseover', function(event){
+    const target = event.target as HTMLElement;
+    switch (target.parentElement?.id) {
+        case 'stacked_image_1':
+            this.style.gridTemplate = 'min-content auto min-content / 41% 13% 13% 13% 13%';
+            break;
+        case 'stacked_image_2':
+            this.style.gridTemplate = 'min-content auto min-content / 13% 41% 13% 13% 13%';
+            break;
+        case 'stacked_image_3':
+            this.style.gridTemplate = 'min-content auto min-content / 13% 13% 41% 13% 13%';
+            break;
+        case 'stacked_image_4':
+            this.style.gridTemplate = 'min-content auto min-content / 13% 13% 13% 41% 13%';
+            break;
+        case 'stacked_image_5':
+            this.style.gridTemplate = 'min-content auto min-content / 13% 13% 13% 13% 41%';
+            break;
+        default:
+            this.style.gridTemplate = 'min-content auto min-content / 41% 13% 13% 13% 13%';
+            break;
+    }
+});
